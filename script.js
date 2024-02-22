728e671614e998ddf74d8ee08ed92e7d76d4c2c9
tyler <tylerjyoungdahl@gmail.com>
2024-02-21 16:02:50 -0800

    forgot to make commits as I worked, oh well

---
+++ script.js
@@ -0,0 +1,54 @@
+function getComputerChoice() {
+  const randNum = Math.floor(Math.random() * 3) + 1;
+  let computerChoice = "";
+  if (randNum === 1) {
+    computerChoice = "rock";
+  } else if (randNum === 2) {
+    computerChoice = "paper";
+  } else if (randNum === 3) {
+    computerChoice = "scissors";
+  }
+  return computerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = prompt("Rock, paper, or scissors?");
+  computerSelection = getComputerChoice();
+  let winner = "";
+
+  if (
+    (playerSelection.toLowerCase() === "rock" &&
+      computerSelection === "scissors") ||
+    (playerSelection.toLowerCase() === "paper" &&
+      computerSelection === "rock") ||
+    (playerSelection.toLowerCase() === "scissors" &&
+      computerSelection === "paper")
+  ) {
+    winner = "player";
+  } else if (
+    (playerSelection.toLowerCase() === "rock" &&
+      computerSelection === "paper") ||
+    (playerSelection.toLowerCase() === "paper" &&
+      computerSelection === "scissors") ||
+    (playerSelection.toLowerCase() === "scissors" &&
+      computerSelection === "rock")
+  ) {
+    winner = "computer";
+  } else if (playerSelection === computerSelection) {
+    winner = "tie";
+  }
+
+  console.log(`You played ${playerSelection}`);
+  console.log(`Computer played ${computerSelection}`);
+  console.log(`Winner: ${winner}`);
+}
+
+function playGame() {
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+}
+
+playGame();
